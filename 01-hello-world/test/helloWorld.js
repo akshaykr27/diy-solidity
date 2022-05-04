24b9af66a7a07462313ddd5314d5667f903c1ae2
Akshay Kumar <akshay.2721989@gmail.com>
2022-05-04 23:14:48 +0530

    refactoring to keep relevant stuff only

--- 01-hello-world/test/helloWorld.js
+++ 01-hello-world/test/helloWorld.js
@@ -11,11 +11,4 @@ contract("HelloWorldContract", () => {
     const res = await smartContract.helloWorld();
     assert(res === "Hello World");
   });
-
-  const _input = "Akshay";
-  it("should return " + _input, async () => {
-    const smartContract = await HelloWorldContract.deployed();
-    const res = await smartContract.greeting(_input);
-    assert(res === _input);
-  });
 });
